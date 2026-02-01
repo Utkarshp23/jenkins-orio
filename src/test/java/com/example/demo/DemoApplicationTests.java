6c9183389854eb13d59f9a26a351e8d7d3b7efb9
Utkarsh Pawar <utkarshpawar2332@gmail.com>
2026-02-01 13:24:45 +0530

    modified test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,15 +3,19 @@ package com.example.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Value("${demo.secret}")
+    private String demoSecret;
+
 	@Test
 	void contextLoads() {
 		// assertEquals(1, 2);
-		System.out.println("Demo Secret in Test: " + System.getProperty("demo.secret"));
+		System.out.println("Demo Secret in Test: " + demoSecret);
 	}
 
 }
